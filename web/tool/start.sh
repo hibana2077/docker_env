b71f4ccdceec0fbda000a5d66ccdb956ff2085ee
hibana2077 <hibana2077@gmail.com>
2024-06-28 13:01:17 +0800

    chore: Update start.sh to create webuser and set password

--- web/tool/start.sh
+++ web/tool/start.sh
@@ -4,9 +4,9 @@
 apt-get update
 apt-get install -y openssh-server curl git vim neofetch
 
-# 創建 mluser 用戶並設置密碼
-useradd -m -s /bin/bash mluser
-echo "mluser:1234" | chpasswd
+# 創建 webuser 用戶並設置密碼
+useradd -m -s /bin/bash webuser
+echo "webuser:1234" | chpasswd
 
 # 配置 SSH 以允許密碼認證
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
@@ -14,12 +14,6 @@ sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_c
 
 # 啟動 SSH 服務
 service ssh start
-
-apt-get install -y python3 python3-pip
-
-pip3 install --upgrade pip
-
-pip3 install -r /tool/requirements.txt
 
 # 保持容器運行
 tail -f /dev/null
